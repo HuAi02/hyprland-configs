6edc897d7ee8e1ffbda78ef78bc604bc05bff6e8
Justin K <103740088+HuAi02@users.noreply.github.com>
2024-07-08 11:19:51 +0800

    Overall improvement
    Took some inspiration from HyprV4 and CTT dotfiles

---
+++ hypr/set_theme.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+. ~/.config/HyprV/set_theme.sh
+
+# Set the xfce and GTK theme
+xfconf-query -c xsettings -p /Net/IconThemeName -s "Papirus-Dark"
+gsettings set org.gnome.desktop.interface icon-theme "Papirus-Dark"
+xfconf-query -c xsettings -p /Net/ThemeName -s "Adwaita-dark"
+gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
